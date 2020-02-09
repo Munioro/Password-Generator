5adea677f65ab55244075be4c36292950d9dbd28
Munioro <munioro27@gmail.com>
2020-02-09 07:47:41 -0600

    added notes, added generatePassword function, Password Generator Worksgit status!

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var passwordGenerated = [];
 var arr = [];
-
+var lngth;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(arr);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -16,47 +15,62 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Prompt user the what requirments the want to meet
 
-var length = prompt("How long do you want the password?\nYou can enter a number between 8 and 128");
-var lowerCase = confirm("Do you want to include lowercase letters?\nClick cancel if no");
-var upperCase = confirm("Do you want to include uppercase letters?\nClick cancel if no");
-var specialChar = confirm("Do you want to include special characters?\nClick cancel if no");
-var numbr = confirm("Do you want to include numbers?\nClick cancel if no");
+//create function to generate password
+function generatePassword(arr){
+  //Prompt user the what requirments they want to meet
+  lngth = prompt("How long do you want the password?\nYou can enter a number between 8 and 128");
 
-if (length >= 8 && length <= 128){
-  for (let i=0; i < length; i++){
-    if (lowerCase == upperCase == specialChar == numbr){
-      arr.push(String.fromCharCode(Math.floor(Math.random()*94)+32))
-    }  
-    }
-}else if (length >= 8 && length <= 128){
-  for (let i=0;i < length; i++){
-    if (lowerCase){
-      // passwordGenerated.push(String.fromCharCode(Math.floor(Math.random()*26)+97));
-      arr.push(String.fromCharCode(Math.floor(Math.random()*26)+97));
-    }
-    if (upperCase){
-      // passwordGenerated.push(String.fromCharCode(Math.floor(Math.random()*26)+65));
-      arr.push(String.fromCharCode(Math.floor(Math.random()*26)+65));
-    }
-    // if (specialChar){
-    //   passwordGenerated.push(String.fromCharCode())
-    // }
-    if (numbr){
-      arr.push(String.fromCharCode(Math.floor(Math.random()*9)+48))
+  //console log variable inputs
+  console.log("The length is "+lngth);
+
+  //if length requirments are met start for loop
+  if (lngth >= 8 && lngth <= 128){
+    var lowerCase = confirm("Do you want to include lowercase letters?\nClick cancel if no");
+    var upperCase = confirm("Do you want to include uppercase letters?\nClick cancel if no");
+    var specialChar = confirm("Do you want to include special characters?\nClick cancel if no");
+    var numbr = confirm("Do you want to include numbers?\nClick cancel if no");
+    //reset to an empty array when function runs
+    arr = [];
+    // console.log("The password is " + generatePassword(arr));
+    console.log("lowerCase="+ lowerCase);
+    console.log("upperCase="+ upperCase);
+    console.log("specialChar="+ specialChar);
+    console.log("number="+ numbr);
+    console.log(arr);
+    console.log(arr.length);
+    console.log(arr.splice(0, length).join(''));
+  
+  
+    //create for loop to generate characters for password
+    for (let i=0; i < lngth; i++){
+       //if lowerCase is true generate characters and push into string
+       if (lowerCase){
+          arr.push(String.fromCharCode(Math.floor(Math.random()*26)+97));
+        }
+        //if upperCase is true generate characters and push into string
+        if (upperCase){
+          arr.push(String.fromCharCode(Math.floor(Math.random()*26)+65));
+        }
+        //if specialChar is true generate characters and push into string
+        if (specialChar){
+          var spArr = [" ","!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+          arr.push(spArr[Math.floor(Math.random()*spArr.length)]);
+        }
+        //if numbr is true generate characters and push into string
+        if (numbr){
+          arr.push(String.fromCharCode(Math.floor(Math.random()*9)+48));
+          }
+    //return arr with wanted length and remove commas from array
+    }return arr.splice(0, lngth).join('');
+
+    
+  //alert length requirments if not met
+  }else {
+    alert("Please enter a number between 8 and 128.");
+    console.log("peramiters not met");
   }
-  }
-}else console.log("peramiters not met")
+}
 
 
-console.log("The length is "+length)
-console.log("lowerCase="+ lowerCase)
-console.log("upperCase="+ upperCase)
-console.log("specialChar="+ specialChar)
-console.log("number="+ numbr)
-// console.log("The password is " +passwordGenerated)
-console.log(arr)
-console.log(arr.length);
-console.log(arr.splice(0, length).join(''));
 
